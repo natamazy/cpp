4ac0d71a804c73cbe30a3290f5c4bb98b36c7b8d
Narek Tamazyan <natamazy@gmail.com>
2024-09-10 18:19:16 +0400

    Megaphone edited

--- module00/ex00/Megaphone.cpp
+++ module00/ex00/Megaphone.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
 
 int main(int argc, char *argv[]) {
-	using std::cout;
-	using std::endl;
-
 	if (argc < 2)
-		cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
 
 	for (int i = 1; argv[i] != NULL; i++) {
 		for (int j = 0; argv[i][j] != '\0'; j++)
-			cout << char(toupper(argv[i][j]));
+			std::cout << char(toupper(argv[i][j]));
 	}
 
-	cout << endl;
+	std::cout << std::endl;
 
 	return 0;
 }
