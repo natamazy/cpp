08dc7d84add58b0cb3559fe71af114ec1d63ae4c
Narek Tamazyan <natamazy@gmail.com>
2024-10-03 16:19:39 +0400

    ex02 done

---
+++ module03/ex02/ClapTrap.hpp
@@ -0,0 +1,27 @@
+#ifndef CLAPTRAP_HPP
+# define CLAPTRAP_HPP
+
+# include <iostream>
+
+class ClapTrap {
+	public:
+		ClapTrap();
+		ClapTrap(std::string name);
+		ClapTrap(const ClapTrap& rhs);
+		
+		virtual		~ClapTrap();
+
+		ClapTrap&	operator=(const ClapTrap& rhs);
+
+		void		attack(const std::string& target);
+		void		takeDamage(unsigned int amount);
+		void		beRepaired(unsigned int amount);
+
+	private:
+		std::string		_name;
+		unsigned int	_hitPoints;
+		unsigned int	_energyPoints;
+		unsigned int	_attackDamage;
+};
+
+#endif
