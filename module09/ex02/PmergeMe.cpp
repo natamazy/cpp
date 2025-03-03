fd120b634a670a50417bac474c4a19fb307e4d01
Narek Tamazyan <natamazy@gmail.com>
2025-03-03 16:48:52 +0400

    cpp09/ex02 added

---
+++ module09/ex02/PmergeMe.cpp
@@ -0,0 +1,90 @@
+#include "PmergeMe.hpp"
+#include <cctype>
+#include <cstddef>
+#include <deque>
+
+PmergeMe::PmergeMe() {}
+PmergeMe::PmergeMe(const PmergeMe &) {}
+PmergeMe &PmergeMe::operator=(const PmergeMe &) { return (*this); }
+PmergeMe::~PmergeMe() {}
+
+static bool chekcOverflow(char *line)
+{
+	if (std::strlen(line) > 11)
+	{
+		return (true);
+	}
+
+	long nbr = std::atol(line);
+
+	return (nbr > INT_MAX || nbr < INT_MIN);
+}
+
+static void check_digit(std::string av)
+{
+	for (size_t i = 0; i < av.length(); i++)
+	{
+		if (!std::isdigit(av[i]))
+		{
+			exit(1 && (std::cout << "Error: invalid input\n"));
+		}
+	}
+}
+
+void PmergeMe::print(int ac, char **av)
+{
+	std::vector<int> arr;
+	std::string line;
+
+	for (int i = 1; i < ac; ++i)
+	{
+		line = av[i];
+
+		if (line.find(" ") != std::string::npos)
+		{
+			std::cout << "Error: input shouldn't contain spaces\n";
+			exit(1);
+		}
+
+		if (chekcOverflow(av[i]))
+		{
+			std::cout << "Error: overflow " << av[i] << "\n";
+			exit(1);
+		}
+
+		check_digit(av[i]);
+
+		int num = std::atoi(av[i]);
+		if (num <= 0)
+		{
+			std::cout << "Error: invalid input\n";
+			return;
+		}
+
+		arr.push_back(num);
+	}
+
+	std::vector<int> vector_arr(arr.begin(), arr.end());
+	std::cout << "Before: ";
+	display(vector_arr);
+
+	clock_t start_time = clock();
+	fordJohnsonSort(vector_arr);
+
+	clock_t end_time = clock();
+	double vector_time = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC;
+
+	std::cout << "After: ";
+	std::deque<int> deque_arr(arr.begin(), arr.end());
+
+	start_time = clock();
+	fordJohnsonSort(deque_arr);
+
+	end_time = clock();
+	display(deque_arr);
+
+	double deque_time = static_cast<double>(end_time - start_time) / CLOCKS_PER_SEC;
+
+	std::cout << "Time to process with vector container: " << std::fixed << std::setprecision(5) << vector_time << " seconds\n";
+	std::cout << "Time to process with deque container:  " << std::fixed << std::setprecision(5) << deque_time << " seconds\n";
+}
---
+++ module09/ex02/PmergeMe.hpp
@@ -0,0 +1,137 @@
+#ifndef PMERGEME_HPP
+#define PMERGEME_HPP
+
+#include <cmath>
+#include <iostream>
+#include <iomanip>
+#include <vector>
+#include <deque>
+#include <algorithm>
+#include <ctime>
+#include <stack>
+
+class PmergeMe
+{
+public:
+	static void print(int argc, char **argv);
+
+private:
+	PmergeMe();
+	PmergeMe(const PmergeMe &);
+	PmergeMe &operator=(const PmergeMe &);
+	~PmergeMe();
+};
+
+template <typename Container>
+void inserion_sort(Container &c)
+{
+	for (size_t i = 1; i < c.size(); i++)
+	{
+		int j = i;
+
+		while (j > 0 && c[j - 1] > c[j])
+		{
+			std::swap(c[j - 1], c[j]);
+			j--;
+		}
+	}
+}
+
+template <typename Container>
+void insert(Container &left, Container &right)
+{
+	size_t n = 0;
+	size_t power = 0;
+	size_t start = 0;
+	size_t end = 0;
+
+	for (size_t i = 0; i < right.size();)
+	{
+		++power;
+
+		n = pow(2, power) - n;
+
+		start += n;
+
+		end = start - n;
+
+		if (start > right.size())
+		{
+			start = right.size();
+		}
+
+		for (size_t j = start - 1; j >= end;)
+		{
+			left.insert(std::upper_bound(left.begin(), left.end(), right[j]), right[j]);
+
+			++i;
+
+			if (j == 0)
+			{
+				break;
+			}
+
+			--j;
+		}
+	}
+}
+
+template <typename Container>
+void fordJohnsonSort(Container &cont)
+{
+	Container left;
+	Container right;
+
+	if (cont.size() < 4)
+	{
+		inserion_sort(cont);
+		return;
+	}
+
+	for (size_t i = 0; i < cont.size() / 2; i++)
+	{
+		int _a = cont[2 * i];
+		int _b = cont[2 * i + 1];
+
+		if (_a > _b)
+		{
+			left.push_back(_b);
+			right.push_back(_a);
+		}
+		else
+		{
+			left.push_back(_a);
+			right.push_back(_b);
+		}
+	}
+
+	if (cont.size() % 2 == 1)
+	{
+		right.push_back(cont[cont.size() - 1]);
+	}
+
+	fordJohnsonSort(left);
+	insert(left, right);
+	cont = left;
+}
+
+template <typename Container>
+void display(const Container &arr)
+{
+	typename Container::const_iterator it = arr.begin();
+	if (it != arr.end())
+	{
+		std::cout << *it;
+		++it;
+	}
+
+	while (it != arr.end())
+	{
+		std::cout << " " << *it;
+		++it;
+	}
+
+	std::cout << std::endl;
+}
+
+#endif
