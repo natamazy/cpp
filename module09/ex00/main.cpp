ad0bbaab78da4fc4177d5f84a34f2e2786bde53b
Narek Tamazyan <natamazy@gmail.com>
2025-03-03 15:54:30 +0400

    fixes in cpp09/ex00

---
+++ module09/ex00/main.cpp
@@ -0,0 +1,21 @@
+#include "BitcoinExchange.hpp"
+#include <exception>
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		std::cout << "Error: invalid numbers of argument" << std::endl;
+		return (1);
+	}
+	BitcoinExchange btc(argv[1]);
+	try
+	{
+		btc.exchange();
+	}
+	catch (const std::exception &e)
+	{
+		std::cout << e.what() << std::endl;
+	}
+	return (0);
+}
